c557387ca38a84ebd64eadcf28e7ab09d4f0c25c
Drew Gordon <drew.s.gordon@gmail.com>
2017-02-21 00:52:05 -0500

    just checking in some half made boilerplate

--- otherothercontent.py
+++ otherothercontent.py
@@ -12,6 +12,9 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 # beautiful soup for parsing
 from bs4 import BeautifulSoup
 
+# multiprocessing for threads
+from multiprocessing import Pool
+
 
 def fetch_sites(PATHTOSITES):
 	fetched_sites = []
@@ -29,14 +32,23 @@ def fetch_sites(PATHTOSITES):
 
 
 
-
-
 if __name__ == '__main__':
 
-	RESOURCES = ARGV[1]
+	RESOURCES = sys.argv[1]
 	USERAGENT_STRING = "Mozilla/5.0 (Linux; U; Android 2.3.3; en-us; LG-LU3000 Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
-
+	ARTICLES_MAX = 3
+	dcap = dict(DesiredCapabilities.PHANTOMJS)
 	dcap['phantomjs.page.settings.userAgent'] = USERAGENT_STRING
-	driver = webdriver.PhantomJS(desired_capabilities=dcap)
+	driver = webdriver.PhantomJS(desired_capabilities=dcap, service_log_path="./logs/ghostdriver.log")
 
-	sites = fetch_sites(RESOURCES)
+	targets = fetch_sites(RESOURCES)
+	
+	for target in targets:
+		# do the stuff to extract the things
+		driver.get(target['site'])
+		soup = BeautifulSoup(driver.page_source, 'html_parser')
+		articles = [i.attrs['href'] for i in soup.select(target['articles_selector'])[0:ARTICLES_MAX]]
+
+	for a in articles:
+
+		# extract.... design this better
